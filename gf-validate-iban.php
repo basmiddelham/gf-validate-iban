bc25c3cb1465e23de0fc5ede34438716ee1fe3c3
Bas Middelham <bas@middelham.nl>
2020-04-23 19:05:53 +0200

    Create gf-validate-iban.php

---
+++ gf-validate-iban.php
@@ -0,0 +1,13 @@
+
+	/**
+	 * GF: Iban validation
+	 */
+	add_filter( 'gform_field_validation', function( $result, $value, $form, $field ) {
+		if ( $field->cssClass === 'verify-iban' ) { // phpcs:ignore
+			if ( ! preg_match( '/[a-zA-Z]{2}[0-9]{2}[a-zA-Z0-9]{4}[0-9]{7}([a-zA-Z0-9]?){0,16}/', $value ) ) {
+				$result['is_valid'] = false;
+				$result['message']  = 'Invalid IBAN';
+			}
+		}
+		return $result;
+	}, 10, 4 );
